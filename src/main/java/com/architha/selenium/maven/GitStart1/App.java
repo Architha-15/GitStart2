a51937e18b16246efe8aa01c62255f70e60bc27c
Architha-15 <architha.gadi93@gmail.com>
2020-05-17 15:53:36 +0530

    staart1

---
+++ src/main/java/com/architha/selenium/maven/GitStart1/App.java
@@ -0,0 +1,13 @@
+package com.architha.selenium.maven.GitStart1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
